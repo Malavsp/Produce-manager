3e42e6b17311c461ef7f85276317c9a5a6683bf7
Malav S Patel <malavpatel2607775@gmail.com>
2024-04-29 21:30:20 -0400

    added root route and error handlers in app.js

---
+++ src/app.js
@@ -0,0 +1,42 @@
+// Imports
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// Create and configure
+const app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Routes
+
+// Root router
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to the Homepage</h1>");
+});
+
+app.get("/error", (req, res) => {
+  throw new Error("Something has gone terribly wrong....");
+});
+
+// Error Handling
+// Unknown Request handler
+app.all("*", (req, res) => {
+  res.status(404).json({
+    msg: "Something is wrong try again later ",
+    reqBody: req.body,
+    reqPath: req.path,
+    reqQuery: req.query,
+    reqMethod: req.method,
+    statusCode: res.statusCode,
+  });
+});
+
+// Internal Error handler
+const handleError = (err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send(" Internal Malfunction  ");
+};
+
+app.use(handleError);
+
+module.exports = app;
